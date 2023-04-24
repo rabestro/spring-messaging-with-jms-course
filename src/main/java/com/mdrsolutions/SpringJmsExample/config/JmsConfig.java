354b82a3d77e775bba95178db736c41c7c715ac7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-04-24 09:33:06 +0300

    feat: Add Message converter for XML messages
    closes #6

--- src/main/java/com/mdrsolutions/SpringJmsExample/config/JmsConfig.java
+++ src/main/java/com/mdrsolutions/SpringJmsExample/config/JmsConfig.java
@@ -1,20 +1,25 @@
 package com.mdrsolutions.SpringJmsExample.config;
 
+import com.mdrsolutions.SpringJmsExample.pojos.Book;
+import com.mdrsolutions.SpringJmsExample.pojos.BookOrder;
+import com.mdrsolutions.SpringJmsExample.pojos.Customer;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MarshallingMessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
 
 @EnableJms
 @Configuration
 public class JmsConfig {
 
-    @Bean
+    //@Bean
     public MessageConverter jacksonJmsMessageConverter(){
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
@@ -23,17 +28,30 @@ public class JmsConfig {
     }
 
     @Bean
+    public MessageConverter xmlMarshallingMessageConverter(){
+        MarshallingMessageConverter converter = new MarshallingMessageConverter(xmlMarshaller());
+        converter.setTargetType(MessageType.TEXT);
+        return converter;
+    }
+    @Bean
+    public XStreamMarshaller xmlMarshaller(){
+        XStreamMarshaller marshaller =  new XStreamMarshaller();
+        marshaller.setSupportedClasses(Book.class, Customer.class, BookOrder.class);
+        return marshaller;
+    }
+
+    @Bean
     public ActiveMQConnectionFactory connectionFactory(){
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin","admin","tcp://localhost:61616");
         return factory;
     }
 
-
     @Bean
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(){
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory());
-        factory.setMessageConverter(jacksonJmsMessageConverter());
+        //factory.setMessageConverter(jacksonJmsMessageConverter());
+        factory.setMessageConverter(xmlMarshallingMessageConverter());
         return factory;
     }
 }
