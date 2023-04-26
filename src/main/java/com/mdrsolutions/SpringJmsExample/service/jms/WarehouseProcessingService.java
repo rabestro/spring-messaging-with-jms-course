739fb30b60e469255a299f5f541d3a6284afa1e1
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-04-26 16:17:14 +0300

    Response management using MessageBuilder sending JMS header
    closes #21

--- src/main/java/com/mdrsolutions/SpringJmsExample/service/jms/WarehouseProcessingService.java
+++ src/main/java/com/mdrsolutions/SpringJmsExample/service/jms/WarehouseProcessingService.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +22,7 @@ public class WarehouseProcessingService {
     private static final Logger LOGGER = LoggerFactory.getLogger(WarehouseProcessingService.class);
 
     @Transactional
-    public ProcessedBookOrder processOrder(BookOrder bookOrder, String orderState, String storeId){
+    public Message<ProcessedBookOrder> processOrder(BookOrder bookOrder, String orderState, String storeId){
 
         if("NEW".equalsIgnoreCase(orderState)){
             return add(bookOrder, storeId);
@@ -34,32 +36,39 @@ public class WarehouseProcessingService {
 
     }
 
-    private ProcessedBookOrder add(BookOrder bookOrder, String storeId){
+    private Message<ProcessedBookOrder> add(BookOrder bookOrder, String storeId){
         LOGGER.info("ADDING A NEW ORDER TO DB");
         //TODO - some type of db operation
-        return new ProcessedBookOrder(
+        return build(new ProcessedBookOrder(
                 bookOrder,
                 new Date(),
                 new Date()
-        );
+        ), "ADDED", storeId);
     }
-    private ProcessedBookOrder update(BookOrder bookOrder, String storeId){
+    private Message<ProcessedBookOrder> update(BookOrder bookOrder, String storeId){
         LOGGER.info("UPDATING A ORDER TO DB");
         //TODO - some type of db operation
-        return new ProcessedBookOrder(
+        return build(new ProcessedBookOrder(
                 bookOrder,
                 new Date(),
                 new Date()
-        );
+        ), "UPDATED", storeId);
     }
-    private ProcessedBookOrder delete(BookOrder bookOrder, String storeId){
+    private Message<ProcessedBookOrder> delete(BookOrder bookOrder, String storeId){
         LOGGER.info("DELETING ORDER FROM DB");
         //TODO - some type of db operation
-        return new ProcessedBookOrder(
+        return build(new ProcessedBookOrder(
                 bookOrder,
                 new Date(),
                 null
-        );
+        ), "DELETED", storeId);
     }
 
+    private Message<ProcessedBookOrder> build(ProcessedBookOrder bookOrder, String orderState, String storeId){
+        return MessageBuilder
+                .withPayload(bookOrder)
+                .setHeader("orderState", orderState)
+                .setHeader("storeId", storeId)
+                .build();
+    }
 }
